f02e87383dc3fffb6f9900bff72cfe614445e424
nick-ricee <nick.rice404@gmail.com>
2025-03-14 15:20:47 -0700

    bruh

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import FacilitiesScreen from './src/screens/FacilitiesScreen';
-
+//nigger
 const Stack = createNativeStackNavigator();
 
 export default function App() {
